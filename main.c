8688dd31ce3016227bf4cd68045d5954779841b6
Felix Moser <felix.moser@informatik.hs-fulda.de>
2025-01-24 17:06:00 +0100

    reading cpu freq

--- main.c
+++ main.c
@@ -21,7 +21,28 @@ void read_cpu_temp() {
     fclose(file);
 }
 
+void read_cpu_frequency() {
+    FILE *file;
+    char buffer[1024];
+    char *freq_path = "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq";
+
+    file = fopen(freq_path, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    if (fgets(buffer, sizeof(buffer), file) != NULL) {
+        float freq = atol(buffer) / 1000000.0f;
+        printf("CPU frequency: %f GHz\n", freq);
+    } else {
+        printf("Error reading file\n");
+    }
+    fclose(file);
+}
+
 int main(void) {
     read_cpu_temp();
+    read_cpu_frequency();
     return 0;
 }
